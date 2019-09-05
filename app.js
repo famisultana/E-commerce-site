6e260d496d685da0a41ebb0d053f159dd2af777e
Fatima Sultana <fami.sultana16@gmail.com>
2019-09-05 20:37:18 +0500

    Integrating css files

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.send('<h1>Page not found!</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3000);
